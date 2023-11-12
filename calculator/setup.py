aaf14147acce64f851e86ea47704901126f475aa
=Arvind Kiwelekar <=awk@dbatu.ac.in>
2023-11-12 13:57:33 +0530

    bug fixed

--- calculator/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_namespace_packages
-
-setup(
-    name="calculator",
-    version="0.0.1",
-    python_requires='>=3',
-    package_dir={'': 'src'},
-    packages=find_namespace_packages(where='src')
-)
--- tests/test_Calculator.py
+++ tests/test_Calculator.py
@@ -1,41 +1,41 @@
 import unittest
-from Calculator import Calculator
+import calculator
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         '''Test case function for addition'''
-        result = Calculator.add(4, 7)
+        result = calculator.add(4, 7)
         expected = 11
         self.assertEqual(result, expected)
 
     def test_substract(self):
         '''Test case function for subtraction'''
 
-        result = Calculator.substract(10, 5)
+        result = calculator.substract(10, 5)
         expected = 5
         self.assertEqual(result, expected)
 
 
     def test_mul(self):
         '''Test case function for multiplication'''
-        result = Calculator.multiply(3, 7)
+        result = calculator.multiply(3, 7)
         expected = 21
         self.assertEqual(result, expected)
 
     def test_div(self):
         '''Test case function for division'''
-        result = Calculator.divide(10, 2)
+        result =calculator.divide(10, 2)
         expected = 5
         self.assertEqual(result, expected)
 
     def test_max(self):
         '''Test case function for max'''
-        result = Calculator.max(10, 2,7)
+        result = calculator.max(10, 2,7)
         expected = 10
         self.assertEqual(result, expected)
-        result = Calculator.max(2,10,7)
+        result = calculator.max(2,10,7)
         expected = 10
         self.assertEqual(result, expected)
-        result = Calculator.max(2,7,10)
+        result = calculator.max(2,7,10)
         expected = 10
         self.assertEqual(result, expected)
